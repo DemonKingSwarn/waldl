6aa28d8b6aebae51e9d4de7a5ff390f543f49471
DemonKingSwarn <rockingswarn@gmail.com>
2023-03-01 10:59:32 +0530

    Chore: fuk windows

--- waldl/__waldl__.py
+++ waldl/__waldl__.py
@@ -19,9 +19,7 @@ def determine_path() -> str:
 
     plt = platform.system()
     
-    if plt == "Windows":
-        return f"C://Users//{os.getenv('username')}//Downloads"
-    elif plt == "Linux" or plt == "Darwin":
+    if plt == "Linux" or plt == "Darwin":
         return expanduser("~/Downloads")
     else:
         print("[!] Unsupported OS")
